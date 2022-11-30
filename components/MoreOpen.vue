908aee592b4823ac6cf19336c56b2ca55bd99f11
Kendall Jackson <kendallkj00@gmail.com>
2022-11-30 17:25:16 -0500

    Test: see if overflow-hidden causes no shadow

--- components/MoreOpen.vue
+++ components/MoreOpen.vue
@@ -15,7 +15,7 @@
       v-click-outside="vcoConfig"
       class="absolute z-10 w-screen max-w-md px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0"
     >
-      <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+      <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
         <div class="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
           <a href="#" class="flex items-start p-3 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
             <!-- Heroicon name: outline/support -->
