1dfa20ac1695a5379c0eb2c00b1dd14979f23166
Kendall Jackson <kendallkj00@gmail.com>
2022-12-19 00:15:04 -0500

    fix: try to force box shadow to render

--- components/MoreOpen.vue
+++ components/MoreOpen.vue
@@ -13,7 +13,7 @@
     <div
       v-if="isOpen"
       v-click-outside="vcoConfig"
-      class="absolute z-40 w-screen max-w-md px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0"
+      class="absolute z-40 rounded-lg shadow-lg w-screen max-w-md px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0"
     >
       <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
         <div class="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
